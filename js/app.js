0620ef67033bedf415b0d98ac0b2bb0ded123634
Alexander Pavlov <alex_pavlov@mail.bg>
2015-02-23 17:06:11 +0200

    Finish animation

--- js/app.js
+++ js/app.js
@@ -2,10 +2,29 @@
 
 var canvas = document.getElementById('canvas');
 
-canvas.width = screen.width;
-canvas.height = screen.height;
+var cWidth  = screen.width;
+var cHeight = screen.height;
+
+canvas.width = cWidth;
+canvas.height = cHeight;
 
 var center = 85;
+var reverse = false;
+
+window.onresize = function(){
+    cWidth  = screen.width;
+    cHeight = screen.height;
+    canvas.width = cWidth;
+    canvas.height = cHeight;
+    if ((center + 35) >= cWidth ) {
+        center = cWidth - 35;
+        reverse = !reverse;
+    } else if ((center - 35) <= 0) {
+        center = 85;
+        reverse = !reverse;
+    };
+
+};
 
 /*var strokeColor = '#000';
 var fillColor = '#fff';
@@ -16,88 +35,62 @@ var ctx = canvas.getContext("2d");
 
 function draw() {
 
+
+    ctx.clearRect(0, 0, cWidth, cHeight);
+
+    // head
     ctx.beginPath();
-    ctx.arc((center - 10), 85,50,0,Math.PI*2,true); // Outer circle
-    //ctx.moveTo(110,75);
+    ctx.arc((center - 10), 85, 50, 0, Math.PI*2, true);
     ctx.stroke();
 
-
+    //body
     ctx.beginPath();
-    ctx.moveTo((center - 10), 125);
-    ctx.lineTo((center - 10), 250); // Outer circle
+    ctx.moveTo((center - 10), 135);
+    ctx.lineTo((center - 10), 250);
     ctx.stroke();
 
     //left hand
     ctx.beginPath();
     ctx.moveTo((center - 60), 125);
-    ctx.lineTo((center - 10), 185); // Outer circle
+    ctx.lineTo((center - 10), 185);
     ctx.stroke();
 
     //left foot
     ctx.beginPath();
     ctx.moveTo((center - 10), 250);
-    ctx.lineTo((center - 60), 325); // Outer circle
+    ctx.lineTo((center - 60), 325);
     ctx.stroke();
-
-
 
     //right hand
     ctx.beginPath();
-    ctx.moveTo(125, 125);
-    ctx.lineTo(75, 185); // Outer circle
+    ctx.moveTo(center + 45, 125);
+    ctx.lineTo(center - 10, 185);
     ctx.stroke();
 
     //right foot
     ctx.beginPath();
-    ctx.moveTo(75, 250);
-    ctx.lineTo(125, 325); // Outer circle
+    ctx.moveTo(center - 10, 250);
+    ctx.lineTo(center + 45, 325);
     ctx.stroke();
 
-    center++;
+    if ((center + 35) >= cWidth || (center - 35) <= 0) {
+        reverse = !reverse;
+        canvas.className = "crash";
+        setTimeout(function(){ canvas.className = ""; }, 1000);
+    };
+
+
+    if (reverse) {
+        center--;    
+    } else{
+        center++;
+    };
 
     window.requestAnimationFrame(draw);
 
-}
-
-window.requestAnimationFrame(draw);
-
-
-
-/*
-function draw(shape) {
-
-    if (shape == 'line') {
-        ctx.beginPath();
-        ctx.moveTo(startX,startY);
-        ctx.lineTo(105,25);
-        ctx.lineTo(25,105);
-        ctx.stroke();  
-    };
-    if (shape == 'circle') {
-        ctx.beginPath();
-        ctx.moveTo(startX,startY);
-        ctx.lineTo(105,25);
-        ctx.lineTo(25,105);
-        ctx.stroke(); 
-    };
-    if (shape == 'square') {
-        ctx.beginPath();
-        ctx.moveTo(startX,startY);
-        ctx.lineTo(105,25);
-        ctx.lineTo(25,105);
-        ctx.stroke();  
-    };
 };
-function drawCircle() {
-    
-};
-function drawSquare() {
-    
-};*/
-
-
 
 
 (function(){
-    //drawLine();
+    window.requestAnimationFrame(draw);
 })();
